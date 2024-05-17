f968edd2416519e97aa5db67f033952f10b68359
Matheus Emiliano Schell dos Santos <matheus.emiliano@hotmail.com>
2024-05-16 21:01:39 -0300

    Criando componente produtos

---
+++ src/app/components/produtos-page/produtos-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './produtos-page.component.html',
+  styleUrl: './produtos-page.component.css'
+})
+export class ProdutosPageComponent {
+
+}
