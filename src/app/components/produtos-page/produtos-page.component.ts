5a31eee77012ef4a17c0d4af09b107ea0840e875
Vinicius Fpach <martelinhonovo@gmail.com>
2024-06-22 21:47:06 -0300

    estilizacao Product Page finalizada.

--- src/app/components/produtos-page/produtos-page.component.ts
+++ src/app/components/produtos-page/produtos-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-produtos-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './produtos-page.component.html',
   styleUrl: './produtos-page.component.css'
 })
