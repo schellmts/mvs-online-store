14f8f5169941795d8e70ea2ab4c201e5eaf0a1e0
schellmts <matheus.emiliano@hotmail.com>
2024-07-02 17:35:19 -0300

    Add viacep

--- src/app/components/rastreio/rastreio.component.ts
+++ src/app/components/rastreio/rastreio.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {FormGroup} from "@angular/forms";
+import {BuscarcepService} from "../../services/buscarcep.service";
 import {HttpClientModule} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-rastreio',
   standalone: true,
-  imports: [NavbarComponent, CommonModule],
+  imports: [NavbarComponent, HttpClientModule, CommonModule, ReactiveFormsModule],
   templateUrl: './rastreio.component.html',
   styleUrl: './rastreio.component.css'
 })
 export class RastreioComponent {
+  form: FormGroup;
+  cepData: any;
 
+  constructor(private fb: FormBuilder, private cepService: BuscarcepService) {
+    this.form = this.fb.group({
+      cep: ['']
+    });
+    this.cepData = null;
+  }
+
+  buscarCep() {
+    const cep = this.form.get('cep')?.value;
+    this.cepService.getCep(cep).subscribe(
+      (data) => {
+        this.cepData = data;
+        console.log(data)
+      },
+      (error) => {
+        console.error(error)
+    })
+  }
 }
